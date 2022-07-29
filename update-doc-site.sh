e8df79ede6e54078718c9b5683a9ee5678191a75
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-30 00:23:34 +0200

    add script to deploy javadoc to github pages

---
+++ update-doc-site.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mvn -DskipTests=true clean install site:site site:stage site:deploy
+cp -rf target/site/apidocs/ mc-config-site/
+mkdir -p mc-config-site/apidocs/resources/fonts/
+touch mc-config-site/apidocs/resources/fonts/dejavu.css
+git -C mc-config-site status
+git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
+
+
