11c02f6b9e83d1361b8be651908f7901667ab5b9
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-31 07:35:26 +0200

    move source doc outside of github page site mc-config-site

--- update-doc-site.sh
+++ update-doc-site.sh
@@ -4,7 +4,9 @@ mvn -DskipTests=true clean install site:site site:stage site:deploy
 cp -rf target/site/apidocs/ mc-config-site/
 mkdir -p mc-config-site/apidocs/resources/fonts/
 touch mc-config-site/apidocs/resources/fonts/dejavu.css
-git -C mc-config-site status
-git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
+
+make -C sourcedoc github
+#git -C mc-config-site status
+#git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
 
 
