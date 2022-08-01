d407e1d3b76bb8c04794144f51d1ada05ba33b6c
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-08-02 00:47:29 +0200

    update remote doc

--- update-doc-site.sh
+++ update-doc-site.sh
@@ -6,7 +6,7 @@ mkdir -p mc-config-site/apidocs/resources/fonts/
 touch mc-config-site/apidocs/resources/fonts/dejavu.css
 
 make -C sourcedoc github
-#git -C mc-config-site status
-#git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
+git -C mc-config-site status
+git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
 
 
