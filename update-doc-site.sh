157d92aa9e141079cf5c1c92c6d2991a6f1cfd54
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-08-04 22:28:26 +0200

    weird

--- update-doc-site.sh
+++ update-doc-site.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-mvn -DskipTests=true clean install site:site site:stage site:deploy
-cp -rf target/site/apidocs/ mc-config-site/
-mkdir -p mc-config-site/apidocs/resources/fonts/
-touch mc-config-site/apidocs/resources/fonts/dejavu.css
+function make_javadoc () {
+	mvn -DskipTests=true clean install site:site site:stage site:deploy
+	cp -rf target/site/apidocs/ mc-config-site/
+	mkdir -p mc-config-site/apidocs/resources/fonts/
+	touch mc-config-site/apidocs/resources/fonts/dejavu.css
+}
 
-make -C sourcedoc github
+# make_javadoc
+make -C sourcedoc clean html github
 git -C mc-config-site status
 git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
 
