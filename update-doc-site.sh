240222dab69253d8448dfe19fed262047fe63b58
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-08-05 23:52:08 +0200

    call javadoc before update site

--- update-doc-site.sh
+++ update-doc-site.sh
@@ -7,7 +7,7 @@ function make_javadoc () {
 	touch mc-config-site/apidocs/resources/fonts/dejavu.css
 }
 
-# make_javadoc
+make_javadoc
 make -C sourcedoc clean html github
 git -C mc-config-site status
 git -C mc-config-site commit -a -m "update apidocs on `date +%Y-%m-%dT%H:%M:%S%z`"
