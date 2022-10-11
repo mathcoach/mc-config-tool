db9531f9d955b501366fab53acb3e493ab1b14fc
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-10-11 12:03:02 +0200

    reformat

---
+++ mc-config-tool-jar-test/src/main/java/de/htwsaar/config/JarTesting.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 hbui.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.htwsaar.config;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author hbui
+ */
+public class JarTesting {
+
+    public static void main(String[] args) throws Exception {        
+        JarTesting jarTest = new JarTesting();
+        for(Path p : jarTest.getPathsFromResourceJAR("/")) {
+            System.out.println(p);
+        }
+    }
+    
+    // Get all paths from a folder that inside the JAR file
+    private List<Path> getPathsFromResourceJAR(String folder) throws URISyntaxException, IOException {
+
+        List<Path> result;
+
+        // get path of the current running JAR
+        String jarPath = getClass().getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .toURI()
+                .getPath();
+        System.out.println("JAR Path :" + jarPath);
+
+        // file walks JAR
+        URI uri = URI.create("jar:file:" + jarPath);
+        try (FileSystem fs = FileSystems.newFileSystem(uri, Collections.emptyMap())) {
+            result = Files.walk(fs.getPath(folder))
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
+        return result;
+
+    }
+
+}
