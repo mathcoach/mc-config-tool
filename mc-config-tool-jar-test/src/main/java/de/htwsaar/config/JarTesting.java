5a2329e5bd1df61f07619c7bf0df246f4502ca46
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2024-12-13 14:20:32 +0100

    update test

--- mc-config-tool-jar-test/src/main/java/de/htwsaar/config/JarTesting.java
+++ mc-config-tool-jar-test/src/main/java/de/htwsaar/config/JarTesting.java
@@ -1,5 +1,6 @@
 package de.htwsaar.config;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -11,8 +12,14 @@ public class JarTesting {
     public static void main(String[] args) {                
         EnvConfiguration conf = new ClasspathBasedConfig("no-existing-config.properties", "jar-testing-config.properties");
         Set<String> config = conf.getAllConfigKeys();
+        Map<String,String> expected = Map.of(
+            "de.htwsaar.config.string", "test",
+            "de.htwsaar.config.number", "123.456",
+            "some.config.param", "some-value",
+            "de.htwsaar.config.path", "/tmp/some/path.txt"
+        );
         config.forEach(key -> 
-            System.out.println("    " + key + " -> " + conf.getConfigValue(key))//NOSONAR System.out in main is OK
+            System.out.println("    " + key + " -> >>" + conf.getConfigValue(key) + "<< expected: >>" + expected.get(key) + "<<")//NOSONAR System.out in main is OK
         );        
     }
 }
